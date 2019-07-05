42b50798be6f180a5d523d488859a4a4ce09ca56
Daniel2 <dgoon9608>
2019-07-05 00:28:11 -0400

    Testing calendar and pickle modules. Worked out saving dictionary to a *.pickle file.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
 import tkinter as tk
 import calendar
+import pickle
 
-root = tk.Tk()
-tk.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry("1280x720")
+    c = calendar.TextCalendar(calendar.SUNDAY)
+    #print(c.formatmonth(2019,6))
+    #for i in c.itermonthdays(2019,6):
+    #    print(i)
+    dict = {2000:"First Birthday"}
+
+    pickle_out = open("test.pickle", "wb")
+    pickle.dump(dict, pickle_out)
+    pickle_out.close()
+
+    pickle_in = open("test.pickle","rb")
+    entry = pickle.load(pickle_in)
+    print(entry[2000])
+    #tk.mainloop()
