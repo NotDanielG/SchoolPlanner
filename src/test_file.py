e70784907463379b616633b20b8a5c0a4c807c33
dgoon <Sbcs111643613>
2019-07-20 22:24:49 -0400

    Error still shows, but got list of day to change its value

--- src/test_file.py
+++ src/test_file.py
@@ -1,9 +1,17 @@
 import tkinter
 
+
 def hello():
     print("hello")
 
+
 class calendar_day(tkinter.Frame):
-    def __init__(self, root, bg, width, height, day):
-        super().__init__(root, bg=bg, width=width, height=height)
+    def __init__(self, root, bg, borderwidth, width, height):
+        tkinter.Frame.__init__(self, root, bg=bg, borderwidth=borderwidth, width=width, height=height)
+        self.day = 0
+
+    def get_day(self):
+        return self.day
+
+    def set_day(self, day):
         self.day = day
