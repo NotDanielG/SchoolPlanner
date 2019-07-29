fa90d38cd2e8ea919154c5b230c7d06b1e73566b
Daniel2 <dgoon9608>
2019-07-29 00:35:14 -0400

    Formatted and added components to subwindow

--- src/tkinter_subclass.py
+++ src/tkinter_subclass.py
@@ -22,34 +22,36 @@ class MakeTaskWindow():
         super().__init__()
         self.task_title = ""
 
-        top = tk.Toplevel()
-        top.geometry("300x400")
-        top.title("Make New Task")
-        label_title = tk.Label(top, text="Title of Task:")
-        label_description = tk.Label(top, text="Task Description:")
-        label_title.grid(row=0, column=0, sticky="E")
-        label_description.grid(row=1, column=0, sticky="NE")
+        self.top = tk.Toplevel()
+        self.top.geometry("300x400")
+        self.top.title("Make New Task")
+        label_title = tk.Label(self.top, text="Title of Task:")
+        label_description = tk.Label(self.top, text="Task Description:")
+        label_title.grid(row=0, column=0, sticky="E", padx=(15, 0))
+        label_description.grid(row=1, column=0, sticky="NE", padx=(15, 0))
 
-        self.entry_title = tk.Entry(top)
+        self.entry_title = tk.Entry(self.top)
         self.entry_title.config(font=("arial", 10))
 
-        self.entry_text = tk.Text(top)
+        textbox_holder = tk.Frame(self.top)
+        print(textbox_holder.winfo_reqwidth())
+        textbox_holder.grid_propagate(True)
+        self.entry_text = tk.Text(textbox_holder)
         self.entry_text.config(font=("arial", 10))
+        self.entry_text.place(x=0, y=0)
 
         self.entry_title.grid(row=0, column=1, sticky="W")
-        self.entry_text.grid(row=1, column=1, sticky="W")
+        textbox_holder.grid(row=1, column=1, sticky="W")
 
-        button = tk.Button(top, text="Save Task", command=self.save)
+        button = tk.Button(self.top, text="Save Task", command=self.save)
 
-        button.place(x=5, y=100)
+        button.place(x=25, y=300)
 
     def set_task_title(self, title):
         self.task_title = title
 
-    def append_task(self, task):
-        self.task.append(task)
-
     def save(self):
-        print("1")
         print(self.entry_title.get())
+        self.task_title = str(self.entry_title.get())
         print(self.entry_text.get("1.0", 'end-1c'))
+        self.top.destroy()
